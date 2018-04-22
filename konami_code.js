29197ef6200f3bd26e716cc02d12b9824b5db117
John Raber <jrab224@gmail.com>
2018-04-22 21:56:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
@@ -19,19 +19,4 @@ function init() {
       }
     }
   });
-  
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
-      index += 1;
-      
-      if (index === alphabet.length) {
-        alert("Hurray!");
-        
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
 }
